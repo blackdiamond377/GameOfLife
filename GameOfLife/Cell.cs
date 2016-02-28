94121c073eeb5ab96382a5e7b489fea7ff1dc2f1
Paul Earhart <earhart.paul@gmail.com>
2016-02-28 11:59:58 -0500

    Initial commit.

---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+
+    class Cell
+    {
+
+        bool _isAlive;
+
+        public Cell(bool isAlive)
+        {
+            this._isAlive = isAlive;
+        }
+
+        public bool isAlive
+        {
+            get
+            {
+                return _isAlive;
+            }
+            set
+            {
+                _isAlive = value;
+            }
+        }
+    }
+}
