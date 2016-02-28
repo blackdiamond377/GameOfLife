2d1bef034c83ca9a49de46c5410af51ee4312108
Paul Earhart <earhart.paul@gmail.com>
2016-02-28 13:48:32 -0500

    Added checking of living neighbors for cells.

--- GameOfLife/Life.cs
+++ GameOfLife/Life.cs
@@ -25,5 +25,33 @@ namespace GameOfLife
                 }
             }
         }
+
+        public void updateBoard()
+        {
+
+        }
+
+        private int getNumNeighbors(int rowNum, int colNum)
+        {
+            int numAlive = 0;
+            int rOffset = rowNum,
+                cOffset = colNum;
+            for(int i = -1; i < 2; i++)
+            {
+                int row = i + rOffset;
+                for(int j = -1; j < 2; j++)
+                {
+                    int col = j + cOffset;
+                    if (row < 0) row = board.GetLength(0) + row; // wraps for -1
+                    if (!(i == 0 && j == 0)) // The center cell isn't its own neighbor
+                    {
+                        if (col < 0) col = board.GetLength(1) + col;
+                        if (board[row, col].isAlive) numAlive++;
+                    }
+                }
+            }
+
+            return numAlive;
+        }
     }
 }
